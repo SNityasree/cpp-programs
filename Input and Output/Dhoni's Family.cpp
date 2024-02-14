506d5f7bc36137f08f571b64435042bb1a5927a2
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 21:01:55 +0530

    Create Dhoni's Family.cpp

---
+++ Input and Output/Dhoni's Family.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"M. S. Dhoni";
+    cout<<"\nFather: Mr. Pan Singh";
+    cout<<"\nMother: Mrs. Devki Devi";
+    cout<<"\nSibings: Narendra Singh and Jayanti Gupta";
+    return 0;
+}
