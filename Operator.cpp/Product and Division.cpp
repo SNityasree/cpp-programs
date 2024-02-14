76c79d6660ec2bb49a7f7f622ad7266133a6a675
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 22:22:03 +0530

    Create Product and Division.cpp

---
+++ Operator.cpp/Product and Division.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,p,q,r;
+    cin>>a>>b;
+    p=(a*b);
+    q=(a/b);
+    r=(a%b);
+    cout<<p<<"\n"<<q<<"\n"<<r;
+    return 0;
+}
