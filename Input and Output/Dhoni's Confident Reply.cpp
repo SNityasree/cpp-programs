760684d1fc9b3677eee6aab924792daf284f00ae
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 21:37:35 +0530

    Update and rename Dhoni's Confident level.cpp to Dhoni's Confident Reply.cpp

---
+++ Input and Output/Dhoni's Confident Reply.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char a[566],b[878];
+    cout<<"Banerjee's Question:\n";
+    cin.getline(a,100);
+    cout<<"Dhoni's Reply:\n";
+    cin.getline(b,100);
+    cout<<"For Banerjee's question \""<<a<< "\" Dhoni's confident reply was \""<<b<<endl;
+}
