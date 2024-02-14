9ce4a1836550001946921f0792028dbac0abea79
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 21:49:46 +0530

    Create Arithmetic Operator.cpp

---
+++ Arithmetic Operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    cout<<a<<" + "<<b<<" = "<<(a+b)<<"\n";
+    cout<<a<<" - "<<b<<" = "<<(a-b)<<"\n";
+    cout<<a<<" * "<<b<<" = "<<(a*b)<<"\n"<<a<<" / "<<b<<" = "<<(a/b)<<"\n"<<a<<" % "<<b<<" = "<<(a%b);
+    
+    return 0;
+}
