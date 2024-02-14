ed6f3451f87d4f1745951bd818d6e5417713629c
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 22:47:42 +0530

    Create Checking Alphabet.cpp

---
+++ Operator.cpp/Checking Alphabet.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   char a;
+   cin>>a; 
+   if((a>='A')&&(a<='Z')){
+       cout<<"Upper";
+   }else if((a>='a')&&(a<='z')){
+       cout<<"Lower";
+   }else if((a>='0')&&(a<='9')){
+       cout<<"Number";
+       
+   }else{
+       cout<<"Symbol";
+   }
+    return 0;
+}
