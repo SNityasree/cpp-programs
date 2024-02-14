4bf27136325fca5376391061d1242d3df74ef315
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 22:34:56 +0530

    Create Fencing The Ground.cpp

---
+++ Operator.cpp/Fencing The Ground.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,x,y;
+    cin>>l>>b;
+    x=((l+b)*2);
+    y=(l*b);
+    cout<<"Required length is "<<x<<"m"<<"\n";
+    cout<<"Required quantity of carpet is "<<y<<"sqm";
+    
+}
