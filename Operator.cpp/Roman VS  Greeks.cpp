eaaf15796cc7a738d3ebe4a82ccc1d52663a8628
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 22:14:37 +0530

    Create  Roman VS  Greeks.cpp

---
+++ Operator.cpp/Roman VS  Greeks.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<(a+(c/2));
+    cout<<" "<<(b+(c/2));
+    //Type your code here
+    return 0;
+}
