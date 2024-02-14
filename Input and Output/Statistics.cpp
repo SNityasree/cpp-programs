a5c51c168e50ae906c8c92956cfd0f50c2433a6a
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 20:54:47 +0530

    Create Statistics.cpp

---
+++ Input and Output/Statistics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cout<<"1)Dead:"<<a;
+    cout<<"\n"<<"2)Injured:"<<b;
+    cout<<"\n"<<"3)Safe:"<<c;
+    cout<<"\n"<<"Please help the people who are suffering!!!";
+    //Type your code here
+    return 0;
+}
