5fcab0b3d9545bb829c239685f07defd558cdc08
SNityasree <159861640+SNityasree@users.noreply.github.com>
2024-02-14 23:11:55 +0530

    Create Savings & Expences.cpp

---
+++ Decision/Savings & Expences.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e;
+    f=b+c+d+e;
+    if(f>a){
+        cout<<"He wants to work hard";
+    }else{
+        cout<<"He can save the money";
+    }
+    return 0;
+}
